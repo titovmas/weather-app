e82d7c79650e990b35b2987ef3065c7ddd6db685
Tigran Tovmasyan <tigrantovmasyan@live.com>
2026-05-14 16:36:41 +0400

    feat: add main loop and user input handling

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("Weather Project")
+import requests
+
+def main():
+    print("--- Welcome to Weather application! ---")
+
+    while True:
+        # Asking user to enter the city
+        city = input("Enter city/postal code (or 'exit' for quit): ").strip()
+
+        # Exit condition for the program
+        if city.lower() == "exit":
+            print("Goodbye. See you again soon")
+            break
+        # Check for empty input
+        if not city:
+            print("Error: city or postal code required")
+            continue
+
+
+if __name__ == "__main__":
+   main()
